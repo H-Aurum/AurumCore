2a8552f0d56856341370c133e7a5a0ac6308c25a
Hebert A. Reis <hebert.afonso@gmail.com>
2025-08-03 17:02:36 -0300

    /aD

--- twitch_integration.py
+++ twitch_integration.py
@@ -1,5 +1,6 @@
 from twitchio import Client
 from twitchio.ext import commands
+import logging
 
 class TwitchChatListener(commands.Bot):
     def __init__(self, token, channel, callback):
@@ -8,17 +9,31 @@ class TwitchChatListener(commands.Bot):
             prefix='!',
             initial_channels=[channel]
         )
+        self.channel = channel
         self.callback = callback
+        self.logger = logging.getLogger('twitch_integration')
 
     async def event_ready(self):
-        print(f'Logged in as | {self.nick}')
+        self.logger.info(f'Logged in to Twitch as | {self.nick}')
+        self.logger.info(f'Monitoring channel | {self.channel}')
 
     async def event_message(self, message):
-        if message.echo:
+        if message.echo or not message.content:
             return
             
-        print(f'Message from {message.author.name}: {message.content}')
-        self.callback(message.author.name, message.content)
+        try:
+            self.callback({
+                'user': message.author.name,
+                'message': message.content,
+                'timestamp': message.timestamp
+            })
+        except Exception as e:
+            self.logger.error(f'Error processing message: {str(e)}')
         
     def start_listener(self):
-        self.run()
+        try:
+            self.run()
+            return True
+        except Exception as e:
+            self.logger.error(f'Failed to start Twitch listener: {str(e)}')
+            return False
